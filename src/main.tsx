a80fd35cef692394c1b4fe0b8bbed68d9802f5fb
niroopn2005-art <niroopn2005@users.noreply.github.com>
2026-03-02 16:13:05 +0530

    Add ConvexProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "next-themes";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import App from "./App.tsx";
 import "./index.css";
 
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider attribute="class">
-      <App />
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider attribute="class">
+        <App />
+      </ThemeProvider>
+    </ConvexProvider>
   </React.StrictMode>,
 );
